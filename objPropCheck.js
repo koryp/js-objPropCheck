e1935a0d8641c6d11779303fca37f295a63ee2c2
Kory <koryp@users.noreply.github.com>
2024-10-25 13:08:51 -0700

    Update objPropCheck.js
    Now supports watching for DOM elements

--- objPropCheck.js
+++ objPropCheck.js
@@ -1,11 +1,13 @@
 /**
- * Sets an interval to check for the existence of a property in an object.
- * If the property is defined, a callback function is executed.
+ * Sets an interval to check for the existence of either:
+ * - a property in an object; or
+ * - an element within a DOM object
+ * If found, a callback function is executed.
  * An option for number of attempts may be set which, if exceeded, executes a
  * failure callback function.
  *
- * @param {Object}   obj  The object that should contain the specified property.
- * @param {string}   prop The property to detect within the specified object.
+ * @param {Object}   obj  The object to watch.
+ * @param {string}   prop property (or element) to detect within the object.
  * @param {Object}   [opt] Options
  * @param {number}   [opt.attempts=0]  Number of attempts. 0 for unlimited.
  * @param {number}   [opt.delay=100]   Miliseconds between each check.
@@ -20,7 +22,8 @@ function objPropCheck(obj, prop, opt) {
     attempts: 0,
     delay: 100,
     debug: false,
-    callback: (args) => console.log("objPropCheck success", args),
+    callback: (result, args) =>
+      console.log("objPropCheck success", result, args),
     failure: (args) => console.log("objPropCheck failure", args),
   }
   const usage = "objPropCheck( <obj> , <prop> [ , <opt> ] )"
@@ -41,12 +44,29 @@ function objPropCheck(obj, prop, opt) {
     if (options.debug) {
       console.log("objPropCheck.check", attempts, args)
     }
-    if (typeof obj[prop] !== "undefined") {
-      clearInterval(interval)
-      options.callback(Object.assign({}, args, { attempts: attempts }))
-    } else if (options.attempts > 0 && attempts >= options.attempts) {
-      clearInterval(interval)
-      options.failure(Object.assign({}, args, { attempts: attempts }))
+    if (typeof obj.querySelectorAll === "function") {
+      let elements = obj.querySelectorAll(prop)
+      if (elements.length > 0) {
+        clearInterval(interval)
+        options.callback(
+          elements,
+          Object.assign({}, args, { attempts: attempts }),
+        )
+      } else if (options.attempts > 0 && attempts >= options.attempts) {
+        clearInterval(interval)
+        options.failure(Object.assign({}, args, { attempts: attempts }))
+      }
+    } else {
+      if (typeof obj[prop] !== "undefined") {
+        clearInterval(interval)
+        options.callback(
+          obj[prop],
+          Object.assign({}, args, { attempts: attempts }),
+        )
+      } else if (options.attempts > 0 && attempts >= options.attempts) {
+        clearInterval(interval)
+        options.failure(Object.assign({}, args, { attempts: attempts }))
+      }
     }
   }
   interval = setInterval(check, options.delay)
